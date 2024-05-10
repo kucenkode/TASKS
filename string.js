0c04d3b30c299af1e5d705970144f018b71ef1cb
Darya <darakucenko542@gmail.com>
2024-05-10 19:06:35 +0300

    [ADD] => complete task1 (output the character of the string) - way_2

--- string.js
+++ string.js
@@ -2,9 +2,10 @@
 let origStr = "Я изучаю JavaScript"
 
 //Способ 1
-function get_the_character_var_1(char_position) {
+console.log("Способ 1: ")
+function get_the_character_way_1(char_position) {
     if (char_position > 0) {
-        let char_output = origStr[char_position - 1] //так как здесь строка представлена массивом, индексация начинается с 0
+        let char_output = origStr[char_position - 1] //индексация начинается с 0
         return char_output
     }
     //Если нужно вывести символ с конца
@@ -18,9 +19,30 @@ function get_the_character_var_1(char_position) {
     }
 }
 
-console.log(get_the_character_var_1(3))
-console.log(get_the_character_var_1(1))
-console.log(get_the_character_var_1(-1))
+console.log(get_the_character_way_1(3))
+console.log(get_the_character_way_1(1))
+console.log(get_the_character_way_1(-1))
 
+//Способ 2
 
+console.log("Способ 2: ")
+function get_the_character_way_2(char_position) {
+    if (char_position > 0) {
+        let char_output = origStr.charAt(char_position - 1)
+        return char_output
+    }
+    //Если нужно вывести символ с конца
+    else if (char_position < 0) {
+        let char_output = origStr.charAt(origStr.length + char_position)
+        return char_output
+    }
+    //Если ввели строку, 0 и тд
+    else {
+        return "Некорректный ввод данных! Попробуйте снова."
+    }
+}
+
+console.log(get_the_character_way_2(3))
+console.log(get_the_character_way_2(1))
+console.log(get_the_character_way_2(-1))
 
