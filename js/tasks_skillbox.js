d2c131f5f79d4bf3887784fd00c50b54f4e9cdc3
Darya Kutsenko <darakucenko542@gmail.com>
2024-06-01 13:19:39 +0300

    [ADD] => add animation to switchTheme button + make static students list dynamic (add function addStudentsToList) + add a button for it(make it animated too)

--- js/tasks_skillbox.js
+++ js/tasks_skillbox.js
@@ -1,11 +1,11 @@
 (function () {
-    (function createStudentsList() {
+    function createStudentsList() {
         let allStudents = [
-            {name: 'Валя', age: 11},
-            {name: 'Таня', age: 24},
-            {name: 'Рома', age: 21},
-            {name: 'Надя', age: 34},
-            {name: 'Антон', age: 7}
+            { name: 'Валя', age: 11 },
+            { name: 'Таня', age: 24 },
+            { name: 'Рома', age: 21 },
+            { name: 'Надя', age: 34 },
+            { name: 'Антон', age: 7 }
         ]
 
         studentsList.style.display = 'none';
@@ -14,7 +14,71 @@
             studentInformation.innerHTML = `<h2>${allStudents[student].name}</h2>` + `<span>Возраст: ${allStudents[student].age} лет</span>`;
             studentsList.appendChild(studentInformation);
         }
-    }) ();
+
+        const addStudent = document.createElement('input');
+        addStudent.type = 'button';
+        addStudent.className = 'addStudent';
+        addStudent.value = '+';
+
+        return {
+            studentsList,
+            addStudent
+        };
+    };
+
+    function addStudentToList() {
+        const addStudentButton = document.querySelector(".addStudent");
+
+        addStudentButton.classList.toggle('addingStudent');
+        if (addStudentButton.type === 'button') {
+            addStudentButton.type = 'text';
+            addStudentButton.value = '';
+            addStudentButton.placeholder = 'Enter the student\'s name and age here...';
+        } else {
+            addStudentButton.value = '+';
+            addStudentButton.type = 'button';
+        };
+
+        addStudentButton.addEventListener('keydown', (event) => {
+            let messageIsShown = false;
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                if (addStudentButton.value.trim() === 0) {
+                    addStudentButton.style.borderColor = 'red';
+                    return;
+                }
+                const newStudent = addStudentButton.value.split(' ');
+                let name, age;
+
+                if (!isNaN(newStudent[0]) && isNaN(newStudent[1])) {
+                    name = newStudent[1];
+                    age = newStudent[0];
+                } else if (isNaN(newStudent[0]) && !isNaN(newStudent[1])) {
+                    name = newStudent[0];
+                    age = newStudent[1];
+                } else {
+                    addStudentButton.style.borderColor = 'red';
+                    const message = document.createElement('span');
+                    message.id = "alertIncorrectInput";
+                    message.textContent = "Please enter name and age of student!";
+                    message.style.color = 'red';
+                    document.querySelector('#containerStudentsList').appendChild(message, addStudentButton);
+                    messageIsShown = true;
+                    return;
+                }
+                const studentInformation = document.createElement('li');
+                studentInformation.innerHTML = `<h2>${name}</h2>` + `<span>Возраст: ${age} лет</span>`;
+                document.querySelector('#studentsList').insertBefore(studentInformation, addStudentButton);
+                addStudentButton.value = "";
+                addStudentButton.style.borderColor = "";
+
+                if (messageIsShown) {
+                    document.querySelector("#alertIncorrectInput").remove();
+                    messageIsShown = false;
+                };
+            };
+        });
+    };
 
     function showStudentsInformation() {
         const studentInformationButton = document.querySelector("#ShowStudentsInfoButton");
@@ -30,11 +94,22 @@
     };
 
     document.addEventListener('DOMContentLoaded', () => {
-        const studentInformationButton = document.querySelector("#ShowStudentsInfoButton");
+        const showStudentInformationButton = document.querySelector("#ShowStudentsInfoButton");
+        const studentsList = document.querySelector('#studentsList');
+        const containerStudentsList = document.querySelector('#containerStudentsList');
 
-        studentInformationButton.addEventListener('click', (event) => {
+        const createList = createStudentsList();
+        containerStudentsList.appendChild(createList.studentsList);
+        studentsList.appendChild(createList.addStudent);
+
+        showStudentInformationButton.addEventListener('click', (event) => {
             event.preventDefault();
             showStudentsInformation();
+        });
+
+        createList.addStudent.addEventListener('click', (event) => {
+            event.preventDefault();
+            addStudentToList();
         });
     });
 }) ()
